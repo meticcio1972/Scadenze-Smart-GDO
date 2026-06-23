869b4ce5b6d2c5944aa7b36f84cb14b2b3387180
meticcio1972 <a.catarinozzi@gmail.com>
2026-06-23 16:47:45 +0200

    Update app.js

--- app.js
+++ app.js
@@ -157,6 +157,21 @@ function modificaScadenza(codice){
     if(!nuovaData) return;
 
     prodotto.scadenza = nuovaData;
+    const parti = nuovaData.split("/");
+
+const dataScad = new Date(
+    parti[2],
+    parti[1]-1,
+    parti[0]
+);
+
+const oggi = new Date();
+oggi.setHours(0,0,0,0);
+
+prodotto.giorni = Math.ceil(
+    (dataScad - oggi) /
+    (1000*60*60*24)
+);
 
     alert("Scadenza aggiornata!");
 
