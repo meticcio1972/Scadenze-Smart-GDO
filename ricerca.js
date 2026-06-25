e2b4720c54b5dd28fc4ca093d3d50ac5881941a8
meticcio1972 <a.catarinozzi@gmail.com>
2026-06-25 22:55:43 +0200

    Update ricerca.js

--- ricerca.js
+++ ricerca.js
@@ -1,19 +1,1 @@
-// ===== RICERCA =====
 
-document.getElementById("ricerca").addEventListener("input", function () {
-
-    const testo = this.value.toLowerCase().trim();
-
-    if (testo === "") {
-        mostraProdotti(prodotti);
-        return;
-    }
-
-    const risultati = prodotti.filter(p =>
-        p.codice.toLowerCase().includes(testo) ||
-        p.descrizione.toLowerCase().includes(testo)
-    );
-
-    mostraProdotti(risultati);
-
-});
