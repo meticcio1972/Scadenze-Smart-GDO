3e317617abc1e58ed97dd5b53eb3aa1ab013d666
meticcio1972 <a.catarinozzi@gmail.com>
2026-06-25 16:30:35 +0200

    Update csv.js

--- csv.js
+++ csv.js
@@ -1,1 +1,29 @@
+// ===== CSV =====
+
+function inizializzaCSV(){
+
+    document.getElementById("csvFile")
+    .addEventListener("change", importaCSV);
+
+}
+
+function importaCSV(e){
+
+    const file = e.target.files[0];
+
+    if(!file) return;
+
+    const reader = new FileReader();
+
+    reader.onload = function(event){
+
+        // Qui sposteremo tutto il codice
+        // dell'importazione CSV
+        // dal vecchio app.js
+
+    };
+
+    reader.readAsText(file);
+
+}
 
