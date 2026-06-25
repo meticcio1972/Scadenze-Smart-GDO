358c3f7b34a15ea03a5f7ec9742e1b65d04516e8
meticcio1972 <a.catarinozzi@gmail.com>
2026-06-25 17:04:10 +0200

    Update scadenze.js

--- scadenze.js
+++ scadenze.js
@@ -28,17 +28,11 @@ storicoModifiche[codice].push({
     pezzi: pezzi,
     dataModifica: new Date().toLocaleString()
 });
-localStorage.setItem(
-    "storicoModifiche",
-    JSON.stringify(storicoModifiche)
-);
+salvaStorico();
 prodotto.scadenza = nuovaData;
     scadenzeModificate[codice] = nuovaData;
 
-localStorage.setItem(
-    "scadenzeModificate",
-    JSON.stringify(scadenzeModificate)
-);
+salvaScadenze();
 
     const parti = nuovaData.split("/");
 
@@ -50,48 +44,10 @@ const dataScad = new Date(
 
 const oggi = new Date();
 oggi.setHours(0,0,0,0);
+prodotto.giorni = calcolaGiorni(nuovaData);
 
-prodotto.giorni = Math.ceil(
-    (dataScad - oggi) /
-    (1000*60*60*24)
-);
 alert("Nuovi giorni: " + prodotto.giorni);
     alert("Scadenza aggiornata!");
-    let scaduti = 0;
-let g3 = 0;
-let g7 = 0;
-let g10 = 0;
-let g15 = 0;
-
-prodotti.forEach(p => {
-
-    if(p.giorni < 0) scaduti++;
-    else if(p.giorni <= 3) g3++;
-    else if(p.giorni <= 7) g7++;
-    else if(p.giorni <= 10) g10++;
-    else if(p.giorni <= 15) g15++;
-
-});
-
-document.querySelector(".scaduti").innerHTML =
-    "⚫ Scaduti<br><strong>" + scaduti + "</strong>";
-
-document.querySelector(".g3").innerHTML =
-    "🔴 Entro 3 giorni<br><strong>" + g3 + "</strong>";
-
-document.querySelector(".g7").innerHTML =
-    "🟠 Entro 7 giorni<br><strong>" + g7 + "</strong>";
-
-document.querySelector(".g10").innerHTML =
-    "🟡 Entro 10 giorni<br><strong>" + g10 + "</strong>";
-
-document.querySelector(".g15").innerHTML =
-    "🟢 Entro 15 giorni<br><strong>" + g15 + "</strong>";
-
-document.querySelector(".totale").innerHTML =
-    "📦 Totale Referenze<br><strong>" +
-    prodotti.length +
-    "</strong>";
-
-    mostraProdotti(prodotti);
+ aggiornaDashboard();
+ mostraProdotti(prodotti);  
 }
