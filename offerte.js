d5671e0a815363ea55ae962addae96f9f2169567
meticcio1972 <a.catarinozzi@gmail.com>
2026-06-25 17:41:13 +0200

    Update offerte.js

--- offerte.js
+++ offerte.js
@@ -1,1 +1,44 @@
+function mettiInOfferta(codice){
 
+    const prodotto =
+    prodotti.find(p => p.codice === codice);
+
+    if(!prodotto){
+        alert("Prodotto non trovato");
+        return;
+    }
+
+    const pezzi = prompt(
+        "Quanti pezzi metti in offerta?",
+        "1"
+    );
+
+    if(!pezzi) return;
+
+    offerte.push({
+        codice: prodotto.codice,
+        descrizione: prodotto.descrizione,
+        scadenza: prodotto.scadenza,
+        pezzi: pezzi,
+        data: new Date().toLocaleString()
+    });
+
+    localStorage.setItem(
+        "offerte",
+        JSON.stringify(offerte)
+    );
+
+    document.querySelector(".offerta").innerHTML =
+        "🔵 Offerta<br><strong>" +
+        offerte.length +
+        "</strong>";
+
+    alert("Prodotto inserito in offerta");
+}
+function esportaVisualizzati(){
+alert("Da esportare: " + prodottiVisualizzati.length);
+  
+    if(prodottiVisualizzati.length === 0){
+        alert("Nessun prodotto da esportare");
+        return;
+    }
