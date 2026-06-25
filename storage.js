853da89773fc767926d3b160f64a1552274e7ac5
meticcio1972 <a.catarinozzi@gmail.com>
2026-06-25 16:25:38 +0200

    Update storage.js

--- storage.js
+++ storage.js
@@ -1,1 +1,45 @@
 
+// ===== STORAGE =====
+
+let prodotti =
+JSON.parse(localStorage.getItem("prodotti")) || [];
+
+let prodottiVisualizzati = [];
+
+let storicoModifiche =
+JSON.parse(localStorage.getItem("storicoModifiche")) || {};
+
+let scadenzeModificate =
+JSON.parse(localStorage.getItem("scadenzeModificate")) || {};
+
+let offerte =
+JSON.parse(localStorage.getItem("offerte")) || [];
+
+
+function salvaProdotti(){
+    localStorage.setItem(
+        "prodotti",
+        JSON.stringify(prodotti)
+    );
+}
+
+function salvaStorico(){
+    localStorage.setItem(
+        "storicoModifiche",
+        JSON.stringify(storicoModifiche)
+    );
+}
+
+function salvaScadenze(){
+    localStorage.setItem(
+        "scadenzeModificate",
+        JSON.stringify(scadenzeModificate)
+    );
+}
+
+function salvaOfferte(){
+    localStorage.setItem(
+        "offerte",
+        JSON.stringify(offerte)
+    );
+}
