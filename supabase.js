879fecda053d81dd1b4fe11b226ff07b757c08bf
meticcio1972 <a.catarinozzi@gmail.com>
2026-06-30 16:46:39 +0200

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -8,7 +8,7 @@ const SUPABASE_URL =
 
 // INCOLLA QUI LA TUA PUBLISHABLE KEY
 const SUPABASE_KEY =
-"INCOLLA_QUI_LA_PUBLISHABLE_KEY";
+"sb_publishable_2SK2XAccJvq_Fn4OVSzwuQ_wlUaEKGO";
 
 const supabase = window.supabase.createClient(
     SUPABASE_URL,
