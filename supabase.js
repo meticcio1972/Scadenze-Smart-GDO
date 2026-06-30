07e92ceba4b06667a27d270449abeed9ed3b9e04
meticcio1972 <a.catarinozzi@gmail.com>
2026-06-30 16:06:32 +0200

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,1 +1,16 @@
+// ==============================
+// SUPABASE.JS
+// Collegamento Scadenze Smart GDO
+// ==============================
 
+const SUPABASE_URL =
+"https://nowomoxfohhbgdnpssds.supabase.co";
+
+// INCOLLA QUI LA TUA PUBLISHABLE KEY
+const SUPABASE_KEY =
+"INCOLLA_QUI_LA_PUBLISHABLE_KEY";
+
+const supabase = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
